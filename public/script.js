f7e1c517bb1637cff117e3998139af65703abe38
JoeOrtiz2003 <127639367+JoeOrtiz2003@users.noreply.github.com>
2025-09-26 16:51:08 +0800

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -73,7 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const mainDiv = document.getElementById("mainDiv");
   mainDiv.style.display = "flex";
 
-  createRankingElements(21);
+  createRankingElements(16);
   fetchRankingData();
   setInterval(fetchRankingData, 1000);
 });
@@ -85,7 +85,7 @@ const sheetURL = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=
 
 let previousRanks = {};
 
-function createRankingElements(count = 21) {
+function createRankingElements(count = 16) {
   const wrapper = document.getElementById("rankingElementsWrapper");
   wrapper.innerHTML = "";
 
